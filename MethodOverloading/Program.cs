86693f6d125d06c31fe0e8faccbbd3fb63e9e0a1
Matthew Bonner <matthewsbonner@comcast.net>
2022-01-19 15:32:51 -0500

    initial commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool c)
+        {
+            if(c == true && a + b == 1)
+            {
+                return $"{a + b} dollar";
+            }
+            else if(c == true && a + b != 1)
+            {
+                return $"{a + b} dollars";
+            }
+            else
+            {
+                return $"{a + b}";
+            }
+        }
+    }
+}
